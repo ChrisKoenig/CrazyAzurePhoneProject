67d1276ae12b6d2318f599fa9e290b2193877e83
Chris Koenig <chris.koenig@microsoft.com>
2011-12-05 22:22:02 -0600

    first commit

---
+++ AzureServices/AWDataService.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+
+namespace AzureServices
+{
+    public class AWDataService : DataService<AdventureWorksLT2008R2Entities>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+            config.SetEntitySetAccessRule("*", EntitySetRights.All);
+            config.MaxResultsPerCollection = 50;
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+        }
+    }
+}
---
+++ PageData.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Services.Client;
+using System.Diagnostics;
+using System.Linq;
+using CrazyAzurePhoneProject.AdventureWorks;
+
+namespace CrazyAzurePhoneProject
+{
+    public class PageData : INotifyPropertyChanged
+    {
+        private DataServiceCollection<Product> _products;
+
+        public DataServiceCollection<Product> DSProducts
+        {
+            get
+            {
+                return _products;
+            }
+            set
+            {
+                _products = value;
+                RaisePropertyChanged("DSProducts");
+                RaisePropertyChanged("Products");
+            }
+        }
+
+        public ObservableCollection<Product> Products
+        {
+            get
+            {
+                return _products;
+            }
+        }
+
+        private AdventureWorksLT2008R2Entities entities;
+
+        public void LoadData()
+        {
+            var uri = new Uri(App.AWSERVICE);
+            entities = new AdventureWorksLT2008R2Entities(uri);
+            DSProducts = new DataServiceCollection<Product>(entities);
+            var query = entities.Products;
+            DSProducts.LoadAsync(query);
+        }
+
+        #region INPC
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+        }
+
+        #endregion INPC
+    }
+}
